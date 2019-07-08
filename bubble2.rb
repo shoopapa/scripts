34f254128be5fe5c20f000d26908f11a9d5154be
shoopapa <joedavis29@gmail.com>
2019-07-07 20:33:44 -0400

    added the regular bubble sort method and added inspect method

--- bubble2.rb
+++ bubble2.rb
@@ -9,6 +9,19 @@ def move_forward(arr, i)
     end
 end
 
+def bubble_sort(arr)
+    (arr.length - 1).times do
+        i = 0
+        (arr.length - 1).times do
+            if arr[i] > arr[i+1]
+                move_forward(arr, i)
+            end
+            i += 1
+        end
+    end
+    arr.inspect
+end
+
 def bubble_by(arr, &block)
     (arr.length - 1).times do
         i = 0
@@ -16,14 +29,14 @@ def bubble_by(arr, &block)
             if block.call(arr[i], arr[i+1]) > 0
                 move_forward(arr, i)
             end
-            puts i += 1
+            i += 1
         end
     end
-    arr
+    arr.inspect
 end
-array = ["hi", "hello", "dookie", "pee", "richie", "jospeh","joey", "joe_davis_is_cool", "ben_is_a_cheap_bastard"]
-
-
-puts bubble_by(array) {|left, right| left.length - right.length}
+array_string = ["hi", "hello", "dookie", "pee", "richie", "jospeh","joey", "joe_davis_is_cool", "ben_is_a_cheap_bastard"]
+array_int = [6,3,4,3674,234,43567,47,47,2,35,25,12,345,75,46,46]
+puts bubble_sort(array_int)
+puts bubble_by(array_string) {|left, right| left.length - right.length}
 
 
