fd5308e35c40d9203da91236d54c61aa9bb9b525
shoopapa <joedavis29@gmail.com>
2019-07-07 20:45:04 -0400

    Comments added and final tests

--- bubble2.rb
+++ bubble2.rb
@@ -1,3 +1,4 @@
+#simply swiches the order of and element in an array with the one right after it
 def move_forward(arr, i)
     if i >= arr.length - 1
         arr
@@ -9,6 +10,8 @@ def move_forward(arr, i)
     end
 end
 
+#runs embedded loops (unoptimized) the inner loop checks if the current index (i) is greater than the one after.
+#if it is greater the move_forward method is called. this procces is repeated untill the array is sorted.
 def bubble_sort(arr)
     (arr.length - 1).times do
         i = 0
@@ -22,6 +25,8 @@ def bubble_sort(arr)
     arr.inspect
 end
 
+#this method uses a block to sort and the main differece is that instead of using the a greater than comparison 
+#for the indexes it uses a block.
 def bubble_by(arr, &block)
     (arr.length - 1).times do
         i = 0
@@ -34,9 +39,11 @@ def bubble_by(arr, &block)
     end
     arr.inspect
 end
+
+#test arrays for sorting
 array_string = ["hi", "hello", "dookie", "pee", "richie", "jospeh","joey", "joe_davis_is_cool", "ben_is_a_cheap_bastard"]
 array_int = [6,3,4,3674,234,43567,47,47,2,35,25,12,345,75,46,46]
 puts bubble_sort(array_int)
 puts bubble_by(array_string) {|left, right| left.length - right.length}
-
+puts bubble_by(array_string) {|left, right| right.length - left.length}
 
